0a5098e10f94bcbab24914c6ca5073c1d2451b90
ziv <ziv@2thepowerof9.com>
2025-01-29 16:12:59 +0200

    changed the app to my own one

--- app.py
+++ app.py
@@ -1,32 +1,15 @@
 import os
+from flask import Flask, send_from_directory
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='build', static_url_path='')
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+def serve_react():
+    return send_from_directory('build', 'index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+@app.route('/<path:path>')
+def serve_static_files(path):
+    return send_from_directory('build', path)
 
 if __name__ == '__main__':
-   app.run()
+    app.run(host='0.0.0.0', port=8000)
